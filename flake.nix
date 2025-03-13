9c7b3e5ed5eac55f74d0b0a85036fd651f0b8f26
Filip Digrín <fdigrin@trask.cz>
2025-03-13 14:07:18 +0100

    Working flake build

--- flake.nix
+++ flake.nix
@@ -18,40 +18,21 @@
         };
         packages = {
           default = packages.lambda;
-          # core = pkgs.buildDotnetModule {
-          #   pname = "dmq-maker-core";
-          #   version = "1.1.0";
-          #
-          #   src = "${self}/DMQCore";
-          #   nugetDeps = builtins.fromJSON (builtins.readFile "Lambda/deps.json");
-          #   projectFile = "DMQCore/DMQCore.csproj";
-          #
-          #   nativeBuildInputs = with pkgs; [
-          #     dotnet-sdk
-          #   ];
-          # };
           lambda = pkgs.buildDotnetModule {
             pname = "dmq-maker-lambda";
             version = "1.0.0";
 
-            src = "${self}";
-            nugetDeps = "${self}/Lambda/deps.json";
-            projectFile = "${self}/Lambda/Lambda.csproj";
+            src = ./.;
+            nugetDeps = ./Lambda/deps.json;
+            projectFile = "./Lambda/Lambda.csproj";
 
-            buildInputs = with pkgs; [
-              dotnet-sdk
-            ];
-
-            # buildPhase = ''
-            #   dotnet build Lambda/Lambda.csproj -c Release --output $out
+            buildType = "Release";
           };
-          # doesnt work
           generate-deps = pkgs.writeShellScriptBin "generateDeps" ''
             git_root=$(git rev-parse --show-toplevel)
             dotnet restore $git_root/Lambda/Lambda.csproj
             ${pkgs.nuget-to-json}/bin/nuget-to-json $git_root/Lambda/packages > $git_root/Lambda/deps.json
           '';
-
         };
       }
     );
